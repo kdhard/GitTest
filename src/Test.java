eef30d78728d11df8e1c8019d9ead44b3b78cd02
mbp2442 <18534447720@163.com>
2026-06-09 14:12:40 +0800

    Revert "two"
    This reverts commit 0336182a760dd4bd87e04581d6a386ff231e8e56.

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,5 @@ import java.sql.SQLOutput;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello,world");
-        System.out.println("toto");
     }
 }
